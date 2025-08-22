f58280c26ae389e464a2a453f596e8152962ba82
Kanishka Katragadda <kkatragadda@ucsd.edu>
2025-08-22 01:34:38 -0700

    Path fix

--- EcoVis Data Collection/CameraView.swift
+++ EcoVis Data Collection/CameraView.swift
@@ -45,7 +45,7 @@ struct CameraView: UIViewControllerRepresentable {
         //Create a new instance
         let picker = UIImagePickerController()
         //Set Coordinator to handle when a picture is taken
-        picker.delegate = /Users/kan/Documents/EcoVis-Data-Collection/EcoVis Data Collection/CameraView.swiftcontext.coordinator
+        picker.delegate = context.coordinator
         //Set source to camera
         picker.sourceType = .camera
         return picker
